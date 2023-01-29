16da228d20d54958fe148875de408df737119bc9
Kurt Salazar <ksalazar@Kurts-MacBook-Air.local>
2023-01-29 18:20:46 -0500

    step 2 - the start page

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, render_template, redirect, flash
+import surveys
+
+app = Flask(__name__)
+
+# list containing user answers
+responses = []
+
+@app.route('/')
+def survey_start():
+    """Generate survey instructions and start page."""
+
+    title = surveys.satisfaction_survey.title
+    instructions = surveys.satisfaction_survey.instructions
+
+    return render_template('base.html', survey_title = title, survey_instructions = instructions)
