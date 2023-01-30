dd0fdf4cb3c7f4361f8d9c2eb307d4263ec93398
Kurt Salazar <ksalazar@kurts-air.home>
2023-01-30 00:04:18 -0500

    fix buttons and complete step 3

--- app.py
+++ app.py
@@ -14,3 +14,15 @@ def survey_start():
     instructions = surveys.satisfaction_survey.instructions
 
     return render_template('home_page.html', survey_title = title, survey_instructions = instructions)
+
+@app.route('/questions/<int:qnum>')
+def show_question(qnum):
+    """Show current question."""
+    
+    title = surveys.satisfaction_survey.title
+    question = surveys.satisfaction_survey.questions[qnum].question
+    choices = surveys.satisfaction_survey.questions[qnum].choices
+    if choices is None:
+        choices = ['Yes', 'No']
+    
+    return render_template('question_page.html', survey_title = title, survey_question = question, survey_choices = choices)
